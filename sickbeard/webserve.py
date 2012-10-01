88c364bb46069ba59837044f5d2837a787a0cc98
Logan Holmes <snown27@gmail.com>
2012-10-01 09:35:04 -0700

    Manually resolving webserve.py conflict

--- sickbeard/webserve.py
+++ sickbeard/webserve.py
@@ -845,7 +845,7 @@ class ConfigPostProcessing:
     def savePostProcessing(self, naming_pattern=None, naming_multi_ep=None,
                     xbmc_data=None, mediabrowser_data=None, synology_data=None, sony_ps3_data=None, wdtv_data=None, tivo_data=None,
                     use_banner=None, keep_processed_dir=None, process_automatically=None, rename_episodes=None,
-                    move_associated_files=None, tv_download_dir=None, naming_custom_abd=None, naming_abd_pattern=None):
+                    move_associated_files=None, tv_download_dir=None, try_hard_link=None, naming_custom_abd=None, naming_abd_pattern=None):
 
         results = []
 
@@ -891,6 +891,7 @@ class ConfigPostProcessing:
         sickbeard.KEEP_PROCESSED_DIR = keep_processed_dir
         sickbeard.RENAME_EPISODES = rename_episodes
         sickbeard.MOVE_ASSOCIATED_FILES = move_associated_files
+        sickbeard.TRY_HARD_LINK = try_hard_link
         sickbeard.NAMING_CUSTOM_ABD = naming_custom_abd
 
         sickbeard.metadata_provider_dict['XBMC'].set_config(xbmc_data)
